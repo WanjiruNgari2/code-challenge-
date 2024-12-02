c7dd3f6fa6ba2ea27fde63818e0968ead443995c
Damaris Ngari <wanjirungari2@gmail.com>
2024-12-02 10:27:12 +0300

    challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -1,7 +1,7 @@
 let num = 20;
-   if (num < 70) {
+   if (num <= 70) {
     console.log('ok');
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i <= 5; i++) {
     console.log('points:2');
     }
 }
